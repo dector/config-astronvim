db90314e4a4fae4178e31fb9fa1c87a7050cfbea
DBot <codebot@dector.space>
2026-04-02 13:10:50 +0000

    feat(keymaps): add movement and reload shortcuts
    Co-authored-by: D <code@dector.space>

--- lua/plugins/pi_keymaps.lua
+++ lua/plugins/pi_keymaps.lua
@@ -5,6 +5,11 @@ return {
     opts = {
       mappings = {
         n = {
+          ["J"] = { "5j", desc = "Jump 5 lines down" },
+          ["K"] = { "5k", desc = "Jump 5 lines up" },
+          ["gh"] = { "<Cmd>normal! K<CR>", desc = "Run keywordprg" },
+          ["<Leader>aj"] = { "J", desc = "Join line with next" },
+          ["<Leader>'r"] = { "<Cmd>AstroReload<CR>", desc = "Reload AstroNvim config" },
           ["<Leader>pv"] = {
             function() vim.cmd "vsplit | terminal pi --extension npm:pi-nvim" end,
             desc = "Open Pi terminal (vertical)",
