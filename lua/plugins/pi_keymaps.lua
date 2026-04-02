dd96083c6ab93747c2fa52aa02df590d33b18868
DBot <codebot@dector.space>
2026-04-02 14:00:54 +0000

    feat(keymaps): remap find mappings ff/fw/fo/fg to sf/sw/so/sg
    Co-authored-by: D <code@dector.space>

--- lua/plugins/pi_keymaps.lua
+++ lua/plugins/pi_keymaps.lua
@@ -15,6 +15,25 @@ return {
             desc = "Open Pi terminal (vertical)",
           },
           ["gp"] = { "<Cmd>PiSend<CR>", desc = "PiSend" },
+
+          -- remap find files
+          ["<Leader>ff"] = false,
+          ["<Leader>sf"] = {
+            function()
+              require("snacks").picker.files {
+                hidden = vim.tbl_get((vim.uv or vim.loop).fs_stat ".git" or {}, "type") == "directory",
+              }
+            end,
+            desc = "Find files",
+          },
+
+          -- remap find words/recent/git files
+          ["<Leader>fw"] = false,
+          ["<Leader>sw"] = { function() require("snacks").picker.grep() end, desc = "Find words" },
+          ["<Leader>fo"] = false,
+          ["<Leader>so"] = { function() require("snacks").picker.recent() end, desc = "Find old files" },
+          ["<Leader>fg"] = false,
+          ["<Leader>sg"] = { function() require("snacks").picker.git_files() end, desc = "Find git files" },
         },
         x = {
           ["gp"] = { ":<C-u>PiSendSelection<CR>", desc = "PiSendSelection" },
