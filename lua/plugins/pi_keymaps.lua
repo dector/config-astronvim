628ff59de7a06891c29a102895fbe4e7c440babd
DBot <codebot@dector.space>
2026-04-02 23:30:23 +0000

    feat(keymaps): add Shift-Esc mapping for next buffer
    Co-authored-by: D <code@dector.space>

--- lua/plugins/pi_keymaps.lua
+++ lua/plugins/pi_keymaps.lua
@@ -18,6 +18,7 @@ return {
           ["[["] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
           ["<C-]>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
           ["<C-[>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+          ["<S-Esc>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
           ["gd"] = { function() vim.lsp.buf.definition() end, desc = "Go to definition" },
           ["gD"] = { function() vim.lsp.buf.declaration() end, desc = "Go to declaration" },
           ["U"] = { "<C-r>", desc = "Redo" },
